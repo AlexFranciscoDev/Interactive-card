25c099efe08b27fe09b16f31a6df0ce28ad81565
unknown <alexbcn98@gmail.com>
2022-10-12 00:54:45 +0200

    css cards

--- interactive_card/src/components/Card.js
+++ interactive_card/src/components/Card.js
@@ -2,14 +2,14 @@ import React from 'react'
 
 export const Card = () => {
     return (
-        <div>
+        <div className="cards">
             <div className="card-front">
                 <div>
                     <img src="/img/card-logo.svg" alt="card-logo" />
                     <div className="card-circle"></div>
                 </div>
-                <p className="card-number">0000 0000 0000 0000</p>
-                <div>
+                <p className="card-number">9591 0000 0000 0000</p>
+                <div className="card-details">
                     <span className="card-user">Felicia Leire</span>
                     <span className="card-expirement">09/26</span>
                 </div>
